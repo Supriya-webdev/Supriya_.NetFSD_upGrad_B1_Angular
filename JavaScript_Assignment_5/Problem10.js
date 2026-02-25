3fddd44c4f40dcbceec18b1f1993d649490e1105
Supriya-webdev <Supriyaa7072@gmail.com>
2026-02-25 22:25:33 +0530

    Add files via upload

---
+++ JavaScript_Assignment_5/Problem10.js
@@ -0,0 +1,17 @@
+class MathUtils {
+  static add(a, b) {
+    return a + b;
+  }
+
+  static subtract(a, b) {
+    return a - b;
+  }
+
+  static multiply(a, b) {
+    return a * b;
+  }
+}
+
+console.log("Add: ", MathUtils.add(10, 20));       
+console.log("Subtract: ", MathUtils.subtract(50, 15)); 
+console.log("Multiply: ", MathUtils.multiply(6, 7));   
